f3f88e242cf7772b6d425832af7c6a42f0e35d9f
leooliveira02 <36467377+leooliveira02@users.noreply.github.com>
2023-01-03 22:05:43 -0300

    install

---
+++ taliSaas/tali-v2-master/frontend/server.js
@@ -0,0 +1,11 @@
+//simple express server to run frontend production build;
+const express = require("express");
+const cors = require('cors')
+const path = require("path");
+const app = express();
+app.use(cors())
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", function (req, res) {
+	res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.listen(3000);
